a37a93a5eb4647c035fa59730f66bdcefc5dc17d
Chadwick Butts <ccbutts@mail.usf.edu>
2022-06-12 03:44:40 -0400

    Changed some data annotations

--- RazorPagesMovie/Models/Movie.cs
+++ RazorPagesMovie/Models/Movie.cs
@@ -7,16 +7,30 @@ namespace RazorPagesMovie.Models
 	public class Movie
 	{
 		public int ID { get; set; }
+
+        [StringLength(60)]
+        [Required]
 		public string Title { get; set; } = string.Empty;
 
         [Display(Name = "Release Date")]
         [DataType(DataType.Date)]
+        [Required]
 		public DateTime ReleaseDate { get; set; }
-		public string Genre { get; set; } = string.Empty;
 
-        [Column(TypeName = "deciaml(18,2)")]
+        [Range(1, 100)]
+        [DataType(DataType.Currency)]
+		[Column(TypeName = "decimal(18,2)")]
+        [Required]
 		public decimal Price { get; set; }
 
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
+        [Required]
+        [StringLength(30)]
+		public string Genre { get; set; } = string.Empty;
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
+        [StringLength(5)]
+        [Required]
 		public string Rating { get; set; } = string.Empty;
 	}
 }
